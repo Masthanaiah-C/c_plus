3f3c67a531b7ac56016a55ef770c05b72f5355af
Masthanaiah-C <ctmasthanaiah5757@gmail.com>
2021-05-08 18:29:06 +0530

    struct

--- day1.cpp
+++ day1.cpp
@@ -1,3 +1,4 @@
+//Bjarne Stroustrup 1979 at Bell labs
 #include <iostream>
 using namespace std;
 
---
+++ day1_1.cpp
@@ -0,0 +1,29 @@
+//Bjarne Stroustrup
+//middle level language
+//object or5iented programmung
+//encapsulation
+//data hiding
+//inheritence
+//polymorphism
+//1979 ; renamed to c++ after 4 years
+//stl standard template library 
+//object,class,methods,instance variables
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "happy days" ;
+	bool bol ;
+	char a;
+	int b;
+	float c;
+	double d;
+	unsigned int e;
+	wchar_t f;
+	cout << sizeof(f) << sizeof(e) << sizeof(int) << endl ;
+	typedef double long2;
+	long2 p;
+	// struct in C
+	
+}
+
